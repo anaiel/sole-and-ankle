93c92f73b2f6b8bafa78e7cfc9557e55934fc46b
anaiel <an.leclabart@gmail.com>
2021-04-09 20:14:57 +0200

    :sparkles: exercise 4 b

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import { formatPrice, pluralize, isNewShoe } from '../../utils';
-import Spacer from '../Spacer';
+import { COLORS, WEIGHTS } from "../../constants";
+import { formatPrice, pluralize, isNewShoe } from "../../utils";
+import Spacer from "../Spacer";
+
+const VARIANTS = {
+  default: {
+    backgroundColor: undefined,
+    content: undefined,
+  },
+  "on-sale": {
+    backgroundColor: COLORS.primary,
+    content: "Sale",
+  },
+  "new-release": {
+    backgroundColor: COLORS.secondary,
+    content: "Just Released!",
+  },
+};
 
 const ShoeCard = ({
   slug,
@@ -29,21 +44,30 @@ const ShoeCard = ({
     ? 'on-sale'
     : isNewShoe(releaseDate)
       ? 'new-release'
-      : 'default'
+      : 'default';
+  const Wrapper = variant === "default" ? DefaultWrapper : LabeledWrapper;
 
   return (
     <Link href={`/shoe/${slug}`}>
-      <Wrapper>
+      <Wrapper
+        style={{
+          "--background-color": VARIANTS[variant].backgroundColor,
+        }}
+        content={VARIANTS[variant].content}
+      >
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price sale={variant === "on-sale"}>{formatPrice(price)}</Price>
         </Row>
         <Row>
-          <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          <ColorInfo>{pluralize("Color", numOfColors)}</ColorInfo>
+          {variant === "on-sale" && (
+            <SalePrice>{formatPrice(salePrice)}</SalePrice>
+          )}
         </Row>
       </Wrapper>
     </Link>
@@ -56,8 +80,23 @@ const Link = styled.a`
   flex: 1 0 340px;
 `;
 
-const Wrapper = styled.article`
+const DefaultWrapper = styled.article`
   padding-bottom: 20px;
+  position: relative;
+`;
+
+const LabeledWrapper = styled(DefaultWrapper)`
+  &:after {
+    content: "${(p) => p.content}";
+    position: absolute;
+    top: 12px;
+    right: -4px;
+    padding: 8px 12px;
+    color: ${COLORS.white};
+    background-color: var(--background-color);
+    border-radius: 2px;
+    font-weight: ${WEIGHTS.medium};
+  }
 `;
 
 const ImageWrapper = styled.div`
@@ -80,7 +119,9 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  ${p => p.sale ? "text-decoration: line-through" : ""}
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
